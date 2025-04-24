2a30aaed162dc6bcc13f4b0c7ceeda61f34fae23
LucasKnight34 <lucasknight336@gmail.com>
2025-04-24 09:18:41 -0600

    Initial Commit

---
+++ FetchRecipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FetchRecipe
+//
+//  Created by Lucas Knight on 4/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FetchRecipe/FetchRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeApp.swift
+//  FetchRecipe
+//
+//  Created by Lucas Knight on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
